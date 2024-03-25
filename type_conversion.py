65ad78d3db6f908a424cef5c73048ab5a25cdf5e
Keshab Kumar Sasmal <keshabsasmal204@gmail.com>
2024-03-25 12:49:47 +0530

    new files are

---
+++ type_conversion.py
@@ -0,0 +1,47 @@
+"""a=100
+b=3
+c=a/b
+print(c)
+print(type(c))"""
+#bool to int
+"""a=int(True)
+print(a)"""
+#string to int
+"""a=(int("100"))
+print(a)"""
+# other to float
+"""a=float(10)
+print(a)"""
+"""a=float(True)
+print(a)
+a=float("hello")
+print(a)"""
+#other to complex
+"""a=complex(200)
+print(a)"""
+a=complex(4.50)
+print(a)
+a=complex(True)
+print(a)
+a=complex("456")
+print(a)
+
+a=bool(300)
+print(a)
+a=bool(33.54)
+print(a)
+a=bool(14+1j)
+print(a)
+a=bool("0")
+print(a)
+a=bool("")
+print(a)
+
+a=str(200)
+print(a)
+a=str(22.76)
+print(a)
+a=str(11+3j)
+print(a)
+a=str(True)
+print(a)
